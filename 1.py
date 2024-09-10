3afb952abd8bcc8b7f697860a6a56e4bc240116a
Katliushkin Sergey <163286339+RBXPrOfIX@users.noreply.github.com>
2024-09-10 18:26:24 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,21 +1,24 @@
-width = float(input("Enter the width: "))
-height = float(input("Enter the height: "))
+width = float(input("Введите ширину: "))
+height = float(input("Введите высоту: "))
+
 
 class Shape:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
+
 class Triangle(Shape):
     def area(self):
-        return 0.5 * self.width * self.height
+        return abs(0.5 * self.width * self.height)
+
 
 class Rectangle(Shape):
     def area(self):
-        return self.width * self.height
+        return abs(self.width * self.height)
+
 
 triangle = Triangle(width, height)
-print("Triangle area:", triangle.area())
-
+print("Площадь треугольника:", triangle.area())
 rectangle = Rectangle(width, height)
-print("Rectangle area:", rectangle.area())
+print("Площадь четырехугольника:", rectangle.area())
